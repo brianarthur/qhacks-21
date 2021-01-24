a4190b117f8b57b902cae74b3e93956b2dd55fea
Brian Arthur <brarthur88@gmail.com>
2021-01-24 00:53:48 -0500

    spotify auth constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,12 @@
-export const spotifyAuthenticateUrl = "https://accounts.spotify.com/authorize";
+// Spotify authentication constants
+const baseUrl = "https://accounts.spotify.com/authorize";
+const clientID = "0413ac0616dc44fb99939db92ba465f3";
+let redirect = process.env.REACT_APP_REDIRECT;
+if (!redirect) redirect = "http://localhost:3000/";
+const scopes = ["playlist-modify-public", "user-read-private", "user-top-read"];
+export const spotifyAuthenticateUrl = `${spotifyAuthenticateUrl}?client_id=${clientID}&response_type=token&redirect_uri=${redirect}&scope=${scopes.join("%20")}`;
 
+// Spotify api urls
 export const spotifySearchUrl = "https://api.spotify.com/v1/search";
 export const spotifyPersonalTracksUrl = "https://api.spotify.com/v1/me/top/tracks";
 export const spotifyGenreRecommendationsUrl = "https://api.spotify.com/v1/recommendations";
@@ -8,5 +15,6 @@ export const spotifyPersonalPlaylistsUrl = "https://api.spotify.com/v1/me/playli
 export const spotifyPlaylistUrl = (id) => `https://api.spotify.com/v1/playlists/${id}/tracks`;
 export const spotifyPopularPlaylistUrl = spotifyPlaylistUrl("1L8BFtWeeHNhNRnL6z6hJl");
 
+// App api urls
 export const generateUrl = "/api/generate";
 export const playPlaylistUrl = (playlistId) => `/api/play/${playlistId}`;
--- src/views/AuthenticateView.js
+++ src/views/AuthenticateView.js
@@ -8,12 +8,6 @@ import Typography from '@material-ui/core/Typography';
 
 import { spotifyAuthenticateUrl } from '../utils/constants';
 
-let clientID = "0413ac0616dc44fb99939db92ba465f3"
-let redirect = "http://localhost:3000/"
-let scopes = ["playlist-modify-public", "user-read-private", "user-top-read"]
-const url = `${spotifyAuthenticateUrl}?client_id=${clientID}&response_type=token&redirect_uri=${redirect}&scope=${scopes.join("%20")}`;
-
-
 const AuthenticateView = () => {
     return (
         <div className="not-authenticated">
@@ -25,7 +19,7 @@ const AuthenticateView = () => {
             </CardContent>
             <CardContent className="not-authenticated-card-btn">
               <CardActions>
-                <Button size="small" color="primary" href={url}>
+                <Button size="small" color="primary" href={spotifyAuthenticateUrl}>
                   Authenticate
                 </Button>
               </CardActions>
